80f582ab5f5733e2f03e75056c24cca2da837668
Khandekar Ved <vedu.khandekar@gmail.com>
2026-05-14 16:23:27 +0530

    Updated ALLOWED_HOSTS for production

--- findly/findly/settings.py
+++ findly/findly/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-pl$_1!wmq)!m%h=8t($f!=)7=07a=#bbm(*ej904kak(+um2+q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['findly-euzv.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
