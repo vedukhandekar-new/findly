c19fcc928032cdeabfe5e35823615271222dfbed
Khandekar Ved <vedu.khandekar@gmail.com>
2026-05-14 16:29:05 +0530

    Fixed CSS styling with WhiteNoise

--- findly/findly/settings.py
+++ findly/findly/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,6 +124,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Tell Django exactly where to look for your CSS files
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
